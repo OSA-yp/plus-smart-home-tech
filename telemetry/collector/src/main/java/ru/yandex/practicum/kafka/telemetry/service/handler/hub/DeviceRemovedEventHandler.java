47bf536ff4835a0860b36aca5a7b071ba544aeb2
Okun Sergey <OkunSA@yandex.ru>
2026-01-08 14:58:31 +0300

    2-collector-grpc

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/service/handler/hub/DeviceRemovedEventHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/service/handler/hub/DeviceRemovedEventHandler.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.kafka.telemetry.service.handler.hub;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.kafka.telemetry.event.DeviceAddedEvent;
 import ru.yandex.practicum.kafka.telemetry.model.hub.events.BaseHubEvent;
 import ru.yandex.practicum.kafka.telemetry.model.hub.events.DeviceRemovedHubEvent;
 import ru.yandex.practicum.kafka.telemetry.model.hub.events.HubEventType;
